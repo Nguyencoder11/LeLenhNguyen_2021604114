92f5aac73feb37320acdca10d72cd7f7d2f8b113
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-08-09 12:26:38 +0700

    :fire: update

---
+++ LeLenhNguyen_2021604114_KTPM03/Project93/Project93/Controllers/DonViController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Project93.Controllers
+{
+    public class DonViController : ApiController
+    {
+        string dbUrl = "Data Source=DESKTOP-4B4DR8B\\SQLEXPRESS;Initial Catalog=QLLuong;Integrated Security=True;Encrypt=False";
+
+        // Web API lay toan bo danh sach don vi
+        [HttpGet]
+        public List<DonVi> LayToanBoDonVi()
+        {
+            QLLuongDBDataContext db = new QLLuongDBDataContext(dbUrl);
+            List<DonVi> donvis = db.DonVis.ToList();
+            foreach (DonVi dv in donvis)
+            {
+                dv.NhanViens = null;
+            }
+            return donvis;
+        }
+
+        // Web API lay chi tiet 1 don vi
+        [HttpGet]
+        public DonVi ChiTietMotDonVi(string madv)
+        {
+            QLLuongDBDataContext db = new QLLuongDBDataContext(dbUrl);
+            DonVi donvi = db.DonVis.FirstOrDefault(x => x.MaDonVi == madv);
+            if (donvi != null)
+            {
+                donvi.NhanViens = null;
+            }
+
+            return donvi;
+        }
+
+        // Web API luu 1 don vi
+        [HttpPost]
+        public bool LuuDonVi(string madv, string tendv)
+        {
+            try
+            {
+                QLLuongDBDataContext db = new QLLuongDBDataContext(dbUrl);
+                DonVi dv = new DonVi
+                {
+                    MaDonVi = madv,
+                    TenDonVi = tendv
+                };
+                db.DonVis.InsertOnSubmit(dv);
+                db.SubmitChanges();
+
+                return true;
+            }
+            catch { }
+            return false;
+        }
+    }
+}
---
+++ LeLenhNguyen_2021604114_KTPM03/Project93/Project93/Controllers/NhanVienController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Project93.Controllers
+{
+    public class NhanVienController : ApiController
+    {
+        string dbUrl = "Data Source=DESKTOP-4B4DR8B\\SQLEXPRESS;Initial Catalog=QLLuong;Integrated Security=True;Encrypt=False";
+
+        // Web API lay toan bo danh sach nhan vien
+        [HttpGet]
+        public List<NhanVien> LayToanBoNhanVien()
+        {
+            QLLuongDBDataContext db = new QLLuongDBDataContext(dbUrl);
+            List<NhanVien> nhanviens = db.NhanViens.ToList();
+            foreach (NhanVien nv in nhanviens) {
+                nv.DonVi = null;
+            }
+
+            return nhanviens;
+        }
+
+        // Web API lay chi tiet 1 nhan vien
+        [HttpGet]
+        public NhanVien ChiTietNhanVien(string manv)
+        {
+            QLLuongDBDataContext db = new QLLuongDBDataContext(dbUrl);
+            NhanVien nv = db.NhanViens.FirstOrDefault(x => x.Ma == manv);
+            if(nv != null)
+            {
+                nv.DonVi = null;
+            }
+            return nv;
+        }
+
+        // Web API lay danh sach nhan vien theo donvi
+        [HttpGet]
+        public List<NhanVien> DanhSachNhanVienTheoDonVi(string madv)
+        {
+            QLLuongDBDataContext db = new QLLuongDBDataContext(dbUrl);
+            List<NhanVien> dsNV = db.NhanViens.Where(x => x.MaDonVi == madv).ToList();
+            foreach(NhanVien nv in dsNV)
+            {
+                nv.DonVi = null;
+            }
+            return dsNV;
+        }
+
+        // Web API lay danh sach nhan vien theo gioi tinh
+        [HttpGet]
+        public List<NhanVien> DanhSachNhanVienTheoGT(string gioitinh)
+        {
+            QLLuongDBDataContext db = new QLLuongDBDataContext(dbUrl);
+            List<NhanVien> dsNV = db.NhanViens.Where(x => x.GioiTinh == gioitinh).ToList();
+            foreach( NhanVien nv in dsNV)
+            {
+                nv.DonVi = null;
+            }
+            return dsNV;
+        }
+
+        // Web API tim ds nhan vien co he so luong trong khoang [a,b]
+        [HttpGet]
+        public List<NhanVien> DanhSachNhanVienCoKhoangLuongAB(decimal a, decimal b)
+        {
+            QLLuongDBDataContext db = new QLLuongDBDataContext(dbUrl);
+            List<NhanVien> dsNV = db.NhanViens.Where(x => x.Hsluong >= a && x.Hsluong <= b).ToList();
+            foreach (NhanVien nv in dsNV)
+            {
+                nv.DonVi = null;
+            }
+            return dsNV;
+        }
+
+
+        // Web API luu 1 nhan vien
+        [HttpPost]
+        public bool LuuNhanVien(string manv, string tennv, DateTime ngaysinh, string gioitinh, decimal hsluong, string madv)
+        {
+            try
+            {
+                QLLuongDBDataContext db = new QLLuongDBDataContext(dbUrl);
+                NhanVien nv = new NhanVien
+                {
+                    Ma = manv,
+                    HoTen = tennv,
+                    NgaySinh = ngaysinh,
+                    GioiTinh = gioitinh,
+                    Hsluong = hsluong,
+                    MaDonVi = madv
+                };
+                db.NhanViens.InsertOnSubmit(nv);
+                db.SubmitChanges();
+
+                return true;
+            }
+            catch { }
+
+            return false;
+        }
+
+
+    }
+}
