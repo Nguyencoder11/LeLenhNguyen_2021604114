2cd021d413e4d6c696f74c6bdf9ffeb78bb3193b
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-08-21 13:44:20 +0700

    :alien: update code for API

---
+++ LeLenhNguyen_2021604114_KTPM03/TaoWebAPI/TrieuGoiWebAPI/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace TrieuGoiWebAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMaSP = new System.Windows.Forms.TextBox();
+            this.txtTenSP = new System.Windows.Forms.TextBox();
+            this.txtDonGia = new System.Windows.Forms.TextBox();
+            this.cboDanhMuc = new System.Windows.Forms.ComboBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.dataDanhSach = new System.Windows.Forms.DataGridView();
+            this.txtMaDM = new System.Windows.Forms.TextBox();
+            this.btnTimDM = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDanhSach)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã sản phẩm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên sản phẩm";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Đơn giá";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Danh mục";
+            // 
+            // txtMaSP
+            // 
+            this.txtMaSP.Location = new System.Drawing.Point(230, 31);
+            this.txtMaSP.Name = "txtMaSP";
+            this.txtMaSP.Size = new System.Drawing.Size(288, 26);
+            this.txtMaSP.TabIndex = 4;
+            // 
+            // txtTenSP
+            // 
+            this.txtTenSP.Location = new System.Drawing.Point(230, 85);
+            this.txtTenSP.Name = "txtTenSP";
+            this.txtTenSP.Size = new System.Drawing.Size(288, 26);
+            this.txtTenSP.TabIndex = 5;
+            // 
+            // txtDonGia
+            // 
+            this.txtDonGia.Location = new System.Drawing.Point(230, 143);
+            this.txtDonGia.Name = "txtDonGia";
+            this.txtDonGia.Size = new System.Drawing.Size(288, 26);
+            this.txtDonGia.TabIndex = 6;
+            // 
+            // cboDanhMuc
+            // 
+            this.cboDanhMuc.FormattingEnabled = true;
+            this.cboDanhMuc.Location = new System.Drawing.Point(230, 198);
+            this.cboDanhMuc.Name = "cboDanhMuc";
+            this.cboDanhMuc.Size = new System.Drawing.Size(288, 28);
+            this.cboDanhMuc.TabIndex = 7;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(698, 30);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(162, 40);
+            this.btnThem.TabIndex = 8;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(698, 100);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(162, 40);
+            this.btnSua.TabIndex = 9;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(698, 165);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(162, 40);
+            this.btnXoa.TabIndex = 10;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // dataDanhSach
+            // 
+            this.dataDanhSach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataDanhSach.Location = new System.Drawing.Point(34, 256);
+            this.dataDanhSach.Name = "dataDanhSach";
+            this.dataDanhSach.RowHeadersWidth = 62;
+            this.dataDanhSach.RowTemplate.Height = 28;
+            this.dataDanhSach.Size = new System.Drawing.Size(773, 182);
+            this.dataDanhSach.TabIndex = 11;
+            this.dataDanhSach.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataDanhSach_CellClick);
+            // 
+            // txtMaDM
+            // 
+            this.txtMaDM.Location = new System.Drawing.Point(365, 460);
+            this.txtMaDM.Name = "txtMaDM";
+            this.txtMaDM.Size = new System.Drawing.Size(225, 26);
+            this.txtMaDM.TabIndex = 12;
+            this.txtMaDM.TextChanged += new System.EventHandler(this.txtMaDM_TextChanged);
+            // 
+            // btnTimDM
+            // 
+            this.btnTimDM.Location = new System.Drawing.Point(626, 457);
+            this.btnTimDM.Name = "btnTimDM";
+            this.btnTimDM.Size = new System.Drawing.Size(181, 33);
+            this.btnTimDM.TabIndex = 13;
+            this.btnTimDM.Text = "Tìm danh mục";
+            this.btnTimDM.UseVisualStyleBackColor = true;
+            this.btnTimDM.Click += new System.EventHandler(this.btnTimDM_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 514);
+            this.Controls.Add(this.btnTimDM);
+            this.Controls.Add(this.txtMaDM);
+            this.Controls.Add(this.dataDanhSach);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.cboDanhMuc);
+            this.Controls.Add(this.txtDonGia);
+            this.Controls.Add(this.txtTenSP);
+            this.Controls.Add(this.txtMaSP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataDanhSach)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMaSP;
+        private System.Windows.Forms.TextBox txtTenSP;
+        private System.Windows.Forms.TextBox txtDonGia;
+        private System.Windows.Forms.ComboBox cboDanhMuc;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.DataGridView dataDanhSach;
+        private System.Windows.Forms.TextBox txtMaDM;
+        private System.Windows.Forms.Button btnTimDM;
+    }
+}
+
---
+++ LeLenhNguyen_2021604114_KTPM03/TaoWebAPI/TrieuGoiWebAPI/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace TrieuGoiWebAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDataGridView();
+            LoadComboBox();
+        }
+
+        public void LoadDataGridView()
+        {
+            // Link goi lay toan bo san pham da cai Web API len IIS
+            string link = "http://localhost/hocrestful/api/sanpham";
+            // Link goi API lay toan bo san pham chua cai Web API len IIS
+            // string link = "https://localhost:44363/api/sanpham";
+
+            // tao mot yeu cau gui toi Website su dung lop HttpWebRequest
+            HttpWebRequest req = WebRequest.CreateHttp(link);
+            // Lay thong tin tu WebServer su dung WebResponse
+            WebResponse res = req.GetResponse();
+            // Tao doi tuong Js kieu mang sang pham theo dinh dang Json
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(SanPham[]));
+            // Lay du lieu tu luong Dl
+            object data = js.ReadObject(res.GetResponseStream());
+            // ep du lieu ve mang san pham
+            SanPham[] arr = data as SanPham[];
+            dataDanhSach.DataSource = arr;
+        }
+
+        public void LoadComboBox()
+        {
+            string link = "http://localhost/hocrestful/api/danhmuc";
+            // string link = "https://localhost:44363/api/danhmuc";
+
+            HttpWebRequest request = WebRequest.CreateHttp(link);
+            WebResponse res = request.GetResponse();
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(DanhMuc[]));
+            object data = js.ReadObject(res.GetResponseStream());
+            DanhMuc[] arr = data as DanhMuc[];
+            cboDanhMuc.DataSource = arr;
+            cboDanhMuc.ValueMember = "MaDanhMuc";
+            cboDanhMuc.DisplayMember = "TenDanhMuc";
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            // chuoi gia tri nhap tu form cho cac TS cua Web API them
+            string postString = string.Format("?ma={0}&ten={1}&gia={2}&madm={3}", txtMaSP.Text, txtTenSP.Text, txtDonGia.Text, cboDanhMuc.SelectedValue);
+            // link goi API them 1 san pham da cai Web API len IIS
+            string link = "http://localhost/hocrestful/api/sanpham/" + postString;
+            // link goi API them 1 san pham chua cai Web API len IIS
+            // string link = "https://localhost:44363/api/sanpham/" + postString;
+
+            HttpWebRequest req = WebRequest.CreateHttp(link);
+            req.Method = "POST";
+            // Tao luong yeu cau
+            Stream dataStream = req.GetRequestStream();
+            // Tao doi tuong js kieu bool theo dinh dang js
+            DataContractJsonSerializer js = new DataContractJsonSerializer (typeof(bool));
+            // lay du lieu tra ve tu luong dl
+            object data = js.ReadObject(req.GetResponse().GetResponseStream());
+            bool kq = (bool)data;
+            if (kq)
+            {
+                LoadDataGridView();
+                MessageBox.Show("Thêm sản phẩm thành công");
+            }
+            else
+            {
+                MessageBox.Show("Thêm sản phẩm thất bại");
+            }
+            clearTextField();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string putString = string.Format("?ma={0}&ten={1}&gia={2}&madm={3}", txtMaSP.Text, txtTenSP.Text, txtDonGia.Text, cboDanhMuc.SelectedValue);
+            string link = "http://localhost/hocrestful/api/sanpham/" + putString;
+            // string link = "https://localhost:44363/api/sanpham/" + putString;
+
+            HttpWebRequest request = WebRequest.CreateHttp(link);
+            request.Method = "PUT";
+
+            Stream dataStream = request.GetRequestStream();
+            DataContractJsonSerializer js = new DataContractJsonSerializer (typeof(bool));
+            object data = js.ReadObject(request.GetResponse().GetResponseStream());
+
+            bool kq = (bool)data;
+            if (kq)
+            {
+                LoadDataGridView();
+                MessageBox.Show("Sửa sản phẩm thành công");
+            }
+            else
+            {
+                MessageBox.Show("Sửa sản phẩm thất bại");
+            }
+            clearTextField();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            string masp = txtMaSP.Text;
+            string deleteString = string.Format("?id={0}", masp);
+            string link = "http://localhost/hocrestful/api/sanpham/" + deleteString;
+            // string link = "https://localhost:44363/api/sanpham/" + deleteString;
+
+            HttpWebRequest req = WebRequest.CreateHttp(link);
+            req.Method = "DELETE";
+            DataContractJsonSerializer js = new DataContractJsonSerializer (typeof(bool));
+            object data = js.ReadObject (req.GetResponse().GetResponseStream());
+            bool kq = (bool)data;
+            if (kq)
+            {
+                LoadDataGridView();
+                MessageBox.Show("Xóa sản phẩm thành công");
+            }
+            clearTextField();
+        }
+
+        private void btnTimDM_Click(object sender, EventArgs e)
+        {
+            string madm = txtMaDM.Text;
+            string link = "http://localhost/hocrestful/api/sanpham?madm=" + madm;
+            //string link = "https://localhost:44363/api/sanpham?madm=" + madm;
+
+            HttpWebRequest req = WebRequest.CreateHttp(link);
+            WebResponse resp = req.GetResponse();
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(SanPham[]));
+            object data = js.ReadObject(resp.GetResponseStream());
+            SanPham[] arr = data as SanPham[];
+            
+            dataDanhSach.DataSource = arr;
+        }
+
+        private void txtMaDM_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMaDM.Text))
+            {
+                LoadDataGridView();
+            }
+        }
+
+        private void dataDanhSach_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int d = e.RowIndex;
+            txtMaSP.Text = dataDanhSach.Rows[d].Cells[0].Value.ToString();
+            txtTenSP.Text = dataDanhSach.Rows[d].Cells[1].Value.ToString();
+            txtDonGia.Text = dataDanhSach.Rows[d].Cells[2].Value.ToString();
+            cboDanhMuc.Text = dataDanhSach.Rows[d].Cells[3].Value.ToString();
+        }
+
+        private void clearTextField()
+        {
+            txtMaSP.Text = null;
+            txtTenSP.Text = null;
+            txtDonGia.Text = null;
+            cboDanhMuc.Text = null;
+        }
+    }
+}
